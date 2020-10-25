e40679548b80a21a24f7aef70cb632fc8f92efda
Erika <erika@istvan.life>
2020-10-25 20:33:23 +0200

    number guess game

--- jatek.rb
+++ jatek.rb
@@ -1,1 +1,18 @@
-puts "Erika Ruby Jateka"
+secret_number = rand(1..100)
+
+guessing = true
+
+while guessing
+	puts
+	print "your guess: "
+	guess = gets.to_i
+
+	if guess == secret_number
+		puts "Congratulations! You won!"
+		guessing = false
+	elsif guess < secret_number
+		puts "Too low.."
+	else
+		puts "Too high ..."
+	end
+end
