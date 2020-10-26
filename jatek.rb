8523282a59064cdf7e90f073666e9f682142f8b9
Erika <erika@istvan.life>
2020-10-26 18:36:03 +0200

    remove unneeded loop

--- jatek.rb
+++ jatek.rb
@@ -5,21 +5,14 @@ secret_number = rand(min_number..max_number)
 run_game = true
 
 while run_game
-	player_number = 0
-	loop do
-		puts
-		print "Talad ki szamot #{min_number} es #{max_number} kozott: "
-		player_input = gets
-		player_number = player_input.to_i
-
-		if player_number >= min_number and player_number <= max_number
-			break
-		end
-
-		puts "Nem jo szamot irtal be: #{player_input}"
-	end
-		
-	if player_number == secret_number
+	puts
+	print "Talad ki szamot #{min_number} es #{max_number} kozott: "
+	player_input = gets
+	player_number = player_input.to_i
+	
+	if player_number < min_number or player_number > max_number
+		puts "Nem jo szamot irtal be: #{player_input}"	
+	elsif player_number == secret_number
 		puts "Ugyes vagy! Kitalaltad a szamot: #{secret_number}"
 		run_game = false
 	else
